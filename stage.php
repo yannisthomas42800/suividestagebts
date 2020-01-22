78dfad75fe920832cfe7bb03123e48abd5ce0c41
Yannis Thomas <yannou42800@gmail.com>
2020-01-22 11:44:27 +0100

    projet init2

--- stage.php
+++ stage.php
@@ -4,9 +4,10 @@ code site
 =======
 <?php
 $nomvisiteur = "dieu";
-$age_du_visiteur = 18;
-echo"le visiteur s'appelle $nomvisiteur et il a $age_du_visiteur ans."
-echo"salut on test tous ça"
+$age_du_visiteur = 8;
+if(§age_du_visiteur <18){
+echo"salut gamin"
+} 
 ?>
 >>>>>>> 803a7b5ae6d548a6766240b13f8929930479c950
 
