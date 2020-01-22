d11d6da30f2569b625590bd4a2b9cf6827b3afca
Alexis-Heyte <alexis.heyte@gmail.com>
2020-01-22 11:24:09 +0100

    Salut c'est la salle

--- stage.php
+++ stage.php
@@ -3,10 +3,10 @@
 code site
 =======
 <?php
-$nomvisiteur = "alexis";
+$nomvisiteur = "dieu";
 $age_du_visiteur = 18;
 echo"le visiteur s'appelle $nomvisiteur et il a $age_du_visiteur ans."
-echo"salut on test tous ça, Va y challah"
+echo"salut on test tous ça"
 ?>
 >>>>>>> 803a7b5ae6d548a6766240b13f8929930479c950
 
